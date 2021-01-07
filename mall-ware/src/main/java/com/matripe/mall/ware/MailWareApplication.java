93cb3ba5dab1c1a0480296852785cd7dfcd168ef
ldd <hi@ldd.im>
2021-01-07 17:54:09 +0800

    首次提交

---
+++ mall-ware/src/main/java/com/matripe/mall/ware/MailWareApplication.java
@@ -0,0 +1,13 @@
+package com.matripe.mall.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailWareApplication.class, args);
+    }
+
+}
